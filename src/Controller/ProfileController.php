f71c46fb006e96f7bfaf88bceb84111641157817
sydelled <syd.elledge@gmail.com>
2024-02-21 15:27:08 -0500

    added profileController and profile twig template

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    #[Route('/profile{id}', name: 'app_profile')]
+    public function show(User $user): Response
     {
-        return $this->render('profile/index.html.twig', [
+        return $this->render('profile/show.html.twig', [
             'controller_name' => 'ProfileController',
+            //pass user object to template
+            'user' => $user
         ]);
     }
 }
